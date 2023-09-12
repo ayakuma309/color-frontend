12d364c9c6792a294bd9e2c0f5d8fd12821c2019
ayakuma309 <ayako.30901@gmail.com>
2023-09-12 23:33:44 +0900

    🔧 add: session provider

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,12 @@
+import { SessionProvider } from 'next-auth/react'
+
+type AuthContextProps = {
+  children: React.ReactNode
+}
+
+// 認証コンテキスト
+function AuthContext({ children }: AuthContextProps) {
+  return <SessionProvider>{children}</SessionProvider>
+}
+
+export default AuthContext
