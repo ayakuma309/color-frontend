771e245152cd79e651095bb491c7fd420956a64a
ayakuma309 <ayako.30901@gmail.com>
2023-09-13 11:43:28 +0900

    ✨ feat: Next.js側からRails APIを叩き、ユーザー情報をDBに保存する

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import axios from 'axios'
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,33 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      const provider = account?.provider
+      const uid = account?.sub
+      const name = user.name
+      const email = user.email
+
+      try {
+        const response = await axios.post(
+          `${apiUrl}/auth/${provider}/callback`,
+          {
+            provider,
+            uid,
+            name,
+            email,
+          }
+        )
+
+        if (response.status === 200) {
+          return true
+        } else {
+          return false
+        }
+      } catch (error) {
+        console.log('エラー', error)
+        return false
+      }
+    },
+  },
 })
